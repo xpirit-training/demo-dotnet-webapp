13228acefbe3c75bca234ee0a8464f2b0bede82e
Till Spindler <tillspi@web.de>
2023-10-06 16:48:02 +0200

    feat: adding nunit test project

---
+++ tests/MvcDemoApp.Tests/ExampleTest.cs
@@ -0,0 +1,15 @@
+namespace MvcDemoApp.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
